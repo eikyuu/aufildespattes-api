0b294c45117c8fe95ad7c3c1fad2643bbbf0d792
eikyuu <v.duguet.dev@gmail.com>
2023-06-25 14:18:42 +0200

    fix slug

--- src/main/java/com/aufildespattes/api/utils/Utils.java
+++ src/main/java/com/aufildespattes/api/utils/Utils.java
@@ -1,18 +1,28 @@
 package com.aufildespattes.api.utils;
 
+import java.text.Normalizer;
+
 public class Utils {
     /**
      * Formats a string into a slug format.
+     * 
      * @param name the string to format
      * @return the formatted slug string
      */
-    public final static String formatSlug(String name) {
-        String slug = name.toLowerCase().replaceAll("[^a-z0-9\\s-]", "").replace(" ", "-");
-        return slug;
+    public final static String formatSlug(String text) {
+        String slug = text.replaceAll(" ", "-");
+        slug = Normalizer.normalize(slug, Normalizer.Form.NFD)
+                .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
+                .replaceAll("[^\\p{ASCII}]", "");
+        slug = slug.replaceAll("[^a-zA-Z0-9-]", "-");
+        slug = slug.replaceAll("-{2,}", "-");
+        slug = slug.replaceAll("^-|-$", "");
+        return slug.toLowerCase();
     }
 
     /**
      * Formats a string into a slug format.
+     * 
      * @param name the string to format
      * @return the formatted slug string
      */
